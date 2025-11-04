efca18b117d96fc1a3555076ba59eec7cf935932
Sven Sutter <sven.sutter@sotax.com>
2025-11-04 12:41:40 +0100

    LoadSprites

---
+++ AnimationTool/CombineSprites/CombineSprites.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace AnimationTool.CombineSprites;
+
+public partial class CombineSprites : UserControl
+{
+    public CombineSprites()
+    {
+        InitializeComponent();
+        DataContext = new CombineSpritesViewModel();
+    }
+}
---
+++ AnimationTool/CombineSprites/CombineSpritesViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+
+namespace AnimationTool.CombineSprites;
+
+public partial class CombineSpritesViewModel : ObservableObject
+{
+    [ObservableProperty] private ImageSource? _imageSource =
+        new BitmapImage(new Uri("pack://application:,,,/Images/Default.png"));
+
+    [ObservableProperty] private ObservableCollection<KeyValuePair<string, string>> _sprites = [];
+    [ObservableProperty] private KeyValuePair<string, string> _selectedSprite;
+    public RelayCommand LoadSpritesCommand { get; }
+    
+    public CombineSpritesViewModel()
+    {
+        LoadSpritesCommand = new RelayCommand(LoadSprites);
+    }
+    
+    private void LoadSprites()
+    {
+        var dlg = new OpenFileDialog()
+        {
+            Filter = "PNG Files (*.png)|*.png",
+            Multiselect = true
+        };
+
+        if (dlg.ShowDialog() == true)
+        {
+            Sprites.Clear();
+            for (int i = 0; i < dlg.FileNames.Length; i++)
+            {
+                Sprites.Add(new KeyValuePair<string, string>(dlg.SafeFileNames[i], dlg.FileNames[i]));
+            }
+        }
+        
+        
+        // dlg.Filter = "PNG Files (*.png)|*.png";
+        // if (dlg.ShowDialog() == true)
+        // {
+        //     var bitmap = new Bitmap(dlg.FileName);
+        //     if (_bitmapSize.Width == 0)
+        //         _bitmapSize = new Size(bitmap.Width, bitmap.Height);
+        //     else if (_bitmapSize.Width != bitmap.Width || _bitmapSize.Height != bitmap.Height)
+        //     {
+        //         MessageBox.Show("Das Image hat eine falsche Grösse");
+        //         return;
+        //     }
+        //
+        //     bitmap.Tag = dlg.FileName;
+        //     _bitmaps.Add(bitmap);
+        //     PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FileNames)));
+        //     PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CanSave)));
+        // }
+            
+    }
+
+    private void ShowPreview(string fileName)
+    {
+        ImageSource = new BitmapImage(new Uri(fileName));
+    }
+    
+    partial void OnSelectedSpriteChanged(KeyValuePair<string, string> value)
+    {
+        ShowPreview(value.Value);
+    }
+}
