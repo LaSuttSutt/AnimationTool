a78135519d439e7aa648a2bd0d5e6ea255bef0e7
Sven Sutter <sven.sutter@sotax.com>
2025-11-18 15:18:10 +0100

    Play Animation

--- AnimationTool/CombineSprites/CombineSpritesViewModel.cs
+++ AnimationTool/CombineSprites/CombineSpritesViewModel.cs
@@ -1,22 +1,22 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 using AnimationTool.Model;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Win32;
-using Point = System.Drawing.Point;
 
 namespace AnimationTool.CombineSprites;
 
 public partial class CombineSpritesViewModel : ObservableObject
 {
-    [ObservableProperty] private ImageSource? _imageSource =
-        new BitmapImage(new Uri("pack://application:,,,/Images/Default.png"));
-
+    [ObservableProperty] private ImageSource? _imageSource; // =
     [ObservableProperty] private ObservableCollection<AnimationBitmap> _sprites = [];
     [ObservableProperty] private AnimationBitmap? _selectedSprite;
     [ObservableProperty] private Stretch _previewStretch = Stretch.None;
+    [ObservableProperty] private string _playBtnText = "Play";
+    [ObservableProperty] private decimal _playSpeed = 0.1m;
+    [ObservableProperty] private bool _loopAnimation;
     [ObservableProperty] private int _ground;
     [ObservableProperty] private int _rows = 1;
     [ObservableProperty] private int _columns = 1;
@@ -27,7 +27,12 @@ public partial class CombineSpritesViewModel : ObservableObject
     public RelayCommand AddRowCommand { get; }
     public RelayCommand RemoveColumnCommand { get; }
     public RelayCommand RemoveRowCommand { get; }
-    
+    public AsyncRelayCommand TogglePlayAnimationCommand { get; }
+    public RelayCommand IncreasePlaySpeedCommand { get; }
+    public RelayCommand DecreasePlaySpeedCommand { get; }
+
+    private bool _playAnimation;
+
     public CombineSpritesViewModel()
     {
         LoadSpritesCommand = new RelayCommand(LoadSprites);
@@ -37,8 +42,12 @@ public partial class CombineSpritesViewModel : ObservableObject
         AddColumnCommand = new RelayCommand(AddColumn);
         RemoveRowCommand = new RelayCommand(RemoveRow);
         RemoveColumnCommand = new RelayCommand(RemoveColumn);
+        TogglePlayAnimationCommand =
+            new AsyncRelayCommand(TogglePlayAnimation, AsyncRelayCommandOptions.AllowConcurrentExecutions);
+        IncreasePlaySpeedCommand = new RelayCommand(IncreasePlaySpeed);
+        DecreasePlaySpeedCommand = new RelayCommand(DecreasePlaySpeed);
     }
-    
+
     private void LoadSprites()
     {
         var dlg = new OpenFileDialog()
@@ -48,7 +57,7 @@ public partial class CombineSpritesViewModel : ObservableObject
         };
 
         if (dlg.ShowDialog() != true) return;
-        
+
         Sprites.Clear();
         for (var i = 0; i < dlg.FileNames.Length; i++)
         {
@@ -60,48 +69,98 @@ public partial class CombineSpritesViewModel : ObservableObject
         }
     }
 
-    private void ShowPreview()
+    private void ShowPreview(AnimationBitmap? selected)
     {
-        ImageSource = SelectedSprite?.ConvertToBitmapImage(Ground);
+        ImageSource = selected?.ConvertToBitmapImage(Ground);
     }
 
     private void IncreaseGround()
     {
         if (Ground < 100)
             Ground += 1;
-        
-        ShowPreview();
+
+        ShowPreview(SelectedSprite);
     }
 
     private void DecreaseGround()
     {
         if (Ground > 0)
             Ground -= 1;
-        
-        ShowPreview();
+
+        ShowPreview(SelectedSprite);
+    }
+
+    private async Task TogglePlayAnimation()
+    {
+        if (!_playAnimation && Sprites.Count > 0)
+        {
+            _playAnimation = true;
+            PlayBtnText = "Stop";
+            await PlayAnimation();
+        }
+        else
+        {
+            _playAnimation = false;
+            PlayBtnText = "Play";
+            if (Sprites.Count > 0)
+                ShowPreview(Sprites[0]);
+        }
+    }
+
+    private void IncreasePlaySpeed()
+    {
+        PlaySpeed += 0.1m;
+    }
+
+    private void DecreasePlaySpeed()
+    {
+        if (PlaySpeed > 0.1m)
+            PlaySpeed -= 0.1m;
     }
 
     private void AddColumn()
     {
-        
     }
-    
+
     private void RemoveColumn()
     {
     }
-    
+
     private void AddRow()
     {
-        
     }
-    
+
     private void RemoveRow()
     {
     }
-    
+
     partial void OnSelectedSpriteChanged(AnimationBitmap? value)
     {
-        SelectedSprite  = value;
-        ShowPreview();
+        SelectedSprite = value;
+        ShowPreview(SelectedSprite);
+    }
+
+    private async Task PlayAnimation()
+    {
+        var index = 0;
+        while (_playAnimation)
+        {
+            ShowPreview(Sprites[index]);
+            await Task.Delay((int)(PlaySpeed * 1000));
+            index++;
+
+            if (index == Sprites.Count)
+            {
+                if (LoopAnimation)
+                    index = 0;
+                else
+                {
+                    _playAnimation = false;
+                    PlayBtnText = "Play";
+                    if (Sprites.Count > 0)
+                        ShowPreview(Sprites[0]);
+                }
+            }
+        }
     }
 }
