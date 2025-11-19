54b067b9ec00d7732af4b19c3d4fd7eede984f57
Sven Sutter <sven.sutter@sotax.com>
2025-11-19 16:46:32 +0100

    Navigation View

---
+++ AnimationTool/Ui/NavigationUc.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace AnimationTool.Ui;
+
+public partial class NavigationUc : UserControl
+{
+    public NavigationUc()
+    {
+        InitializeComponent();
+        DataContext = new NavigationUcViewModel();
+    }
+}
---
+++ AnimationTool/Ui/NavigationUcViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Controls;
+using AnimationTool.CombineSprites;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace AnimationTool.Ui;
+
+public partial class NavigationUcViewModel : ObservableObject
+{
+    [ObservableProperty] private UserControl _selectedPage = new();
+    public RelayCommand NavToAnimationsCommand { get; }
+
+    private readonly UserControl _animationsUc = new CombineSprites.CombineSprites();
+
+    public NavigationUcViewModel()
+    {
+        NavToAnimationsCommand = new RelayCommand(NavigateToAnimations);
+        _animationsUc.DataContext = new CombineSpritesViewModel();
+    }
+
+    private void NavigateToAnimations()
+    {
+        SelectedPage = _animationsUc;
+    }
+}
