6ee623858a2dcf468b4ecfbc4c9510f742daf7d2
Sven Sutter <sven.sutter@sotax.com>
2025-12-08 13:30:23 +0100

    Diverses

---
+++ AnimationTool/Ui/_Controls/TextBoxUc.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AnimationTool.Ui._Controls;
+
+public partial class TextBoxUc
+{
+    public TextBoxUc()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+        nameof(Text), typeof(string), typeof(TextBoxUc), new PropertyMetadata("Fallback"));
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+}
